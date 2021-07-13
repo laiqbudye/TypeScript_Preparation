34bdcf248e4fc421f0bf2789b13e186f4af7936d
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-13 20:36:49 +0530

    Update 1. Intro.js

--- Concepts/1. Intro.js
+++ Concepts/1. Intro.js
@@ -1,1 +1,23 @@
 
+What is Typescript?
+  
+  Typescript = Javascript + A type System
+
+
+What is type system?
+  - Helps us to catch errors during development
+  - uses 'type annotations' to analyze our code
+  - only active during development
+
+
+here, we need to convert our typescript code into JS as browser does not understand typescript.
+
+
+      Typescript code           ====>     Typescript Compiler     =====>     Plain old Javascript
+ (JS with type annotations)
+
+
+
+Writing Typescript is the same as writing Javascript with some "extra documentation".
+           
+           
