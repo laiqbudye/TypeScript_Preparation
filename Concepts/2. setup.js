ad882959c65285f4ca72e27cb0592c780082b209
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-13 20:48:51 +0530

    Create 2. setup.js

---
+++ Concepts/2. setup.js
@@ -0,0 +1,6 @@
+to work with typescript we need to setup that first.
+
+  - to install 
+    npm i typescript
+    
+    this installation will give us access to 'tsc' command which stands for typescript compiler.
