e3cc6eb75c8f256b599d25d8ac90028c7d8a5ee3
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-13 21:04:08 +0530

    Update 2. setup.js

--- Concepts/2. setup.js
+++ Concepts/2. setup.js
@@ -8,3 +8,16 @@ to work with typescript we need to setup that first.
     
     so to convert our typescript (.ts) file into .js file
       - tsc index.ts      // this command will generate index.js file which can be understandable by the browser.
+      - node index.js     // which will show output on terminal
+
+
+  but in the above approach we need to run 2 different commands
+  so to avoid that we can make use of 'ts-node'  pkg
+  
+    - to install
+     npm i ts-node
+
+
+so the same process with ts-node pkg will be
+
+    - ts-node index.ts    // here it will convert ts file to js file & also execute that newly generated js file in one go (no need to again xecute that js file)
