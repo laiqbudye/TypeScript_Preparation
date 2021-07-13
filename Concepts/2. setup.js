c4d6805fa138a4af9fcc8f8d1544d91afd7a82ef
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-13 20:58:52 +0530

    Update 2. setup.js

--- Concepts/2. setup.js
+++ Concepts/2. setup.js
@@ -4,3 +4,7 @@ to work with typescript we need to setup that first.
     npm i typescript
     
     this installation will give us access to 'tsc' command which stands for typescript compiler.
+    
+    
+    so to convert our typescript (.ts) file into .js file
+      - tsc index.ts      // this command will generate index.js file which can be understandable by the browser.
