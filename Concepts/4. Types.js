1da9130735ea3a9fd599ed06b0397024fbc40fc5
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-14 00:16:05 +0530

    Create 4. Types.js

---
+++ Concepts/4. Types.js
@@ -0,0 +1,20 @@
+What is Type in TS?
+  by definition:- easy way to refer to the different properties + functions that a value has.
+  value is aything that we assign to a variable. (number, string, boolean, undefined, null)
+
+  var a = 'red';   // a is now a string type which has access to methods like charAt(), length.
+
+
+Types
+1. Primitive types - number, string, boolean, undefined, null, void, symbol
+2. Object types - functions, arrays, classes, objects
+
+
+
+Why do we care about types?
+  - types are used by typescript compiler to analyze our code for errors.
+  - allows other devs to understand what values are flowing around our codebase.
+
+
+  
+  
