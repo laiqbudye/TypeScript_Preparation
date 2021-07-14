953d47154da37d3930b92cc76005ab690b3a8dab
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-14 12:32:33 +0530

    Update 6. Type_Inference.js

--- Concepts/6. Type_Inference.js
+++ Concepts/6. Type_Inference.js
@@ -21,7 +21,7 @@ when to use
 
 e.g  // when to use annotations
 
-// 1. Functions that returns 'any' type
+// i. Functions that returns 'any' type
 
 const json = '{"x": 10, "y": 20}';
 
@@ -36,5 +36,64 @@ coordinates.something;    // here TS will now throw error saying "something does
 
 
 
+// ii. when we declare a variable on one line & initialize it later
+
+let words = ['red', 'green', 'blue'];
+let foundword;       // here we dont initialize varibale on same line so type inference wont work here. it provides 'any' type
+
+for(let i = 0; i<words.length; i++) {
+    if(words[i] === 'green'){
+        foundword = true;
+    }
+}
+
+
+// so to solve this
+
+let words1 = ['red', 'green', 'blue'];
+let foundword1: boolean;     or    foundword1 = false;
+
+for(let i = 0; i<words1.length; i++) {
+    if(words1[i] === 'green'){
+        foundword1 = true;
+    }
+}
+
+
+
+//iii. varibale whose type cannot be inferred.
+
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;      // here the variable can store boolean or number
+
+for (let i = 0; i < numbers.length; i++) {
+    if(numbers[i] > 0){
+        numberAboveZero = numbers[i];      // so here numberAboveZero is boolean & we are trying to store number inside it... so to fix that we can use OR '|' operator with declaration
+    }
+}
+
+
+
 
 2. type inference  - always
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
